86a20d8e1286dd50cd87269ea2080c4c5211e1b2
Wuilmer  Medrano <wuilmerj24@wuilmerj24-pc.home>
2025-08-04 20:23:15 -0600

    FIX:url repos

---
+++ packages/country-code/angular/index.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { registerElement } from '@nativescript/angular';
+import { CountryCode } from '@wuilmerj24/country-code';
+
+@NgModule()
+export class NativeScriptCountryCodeModule {}
+
+// Uncomment this line if the package provides a custom view component
+// registerElement('CountryCode', () => CountryCode);
